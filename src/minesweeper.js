a1d3a2d5b15475a33c1d6eddedd74fcaaced89f5
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-09-08 08:10:14 -0700

    Adding JS file

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -17,4 +17,4 @@ const bombLine = '  | B |  ';
 console.log('This is what a board with a guess and a bomb on it would look like:');
 console.log(guessLine);
 console.log(bombLine);
-console.log(blankline);
+console.log(blankLine);
