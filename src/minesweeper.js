b77afee0423fac6f8cdd22e24307c5c1ad38b0ef
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-09-11 10:06:19 -0700

    Created the new minesweeper board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,19 @@
+// Printing the board to the console
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
 // Creating the game board using an array
 const board = [
   [ ' ', ' ', ' ' ],
   [ ' ', ' ', ' ' ],
   [ ' ', ' ', ' ' ]
 ];
-console.log(board);
+
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
